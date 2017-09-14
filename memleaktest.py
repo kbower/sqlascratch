f7198b898bc7299dea459c9a7aad929701613000
Kent Bower <kb@retailarchitects.com>
2017-09-14 07:50:57 -0400

    Develop test PR (#6)
    * spacing
    
    * correct indents
    
    * space foreign.py
    
    * indent better
    
    * Update memleaktest.py
    
    * commit what will cause PR conflict
    
    * add comments

--- memleaktest.py
+++ memleaktest.py
@@ -3,7 +3,6 @@
 # please refer to http://www.sqlalchemy.org/trac/ticket/2427
 # Author: Iuri Diniz
 import gc
-
 from sqlalchemy import create_engine, Table, __version__
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, Unicode, String
@@ -19,6 +18,7 @@ engine = create_engine(url)
 Base = declarative_base()
 Session = sessionmaker(bind=engine)
 
+
 class Config(Base):
     __tablename__ = "config"
 
@@ -27,10 +27,12 @@ class Config(Base):
     value = Column(Unicode(256))
 
     def __repr__(self):
-        return "%s.%s(id=%r, key=%r, value=%r)" % (self.__class__.__module__,
-                                                   self.__class__.__name__,
-                                                   self.id, self.key,
-                                                   self.value)
+        return "%s.%s(id=%r, key=%r, value=%r)" % (
+            self.__class__.__module__,
+            self.__class__.__name__,
+            self.id, self.key,
+            self.value)
+
 
 log_table = Table('log_table', Base.metadata,
     Column('id', Integer, primary_key=True),
@@ -45,6 +47,7 @@ log_table = Table('log_table', Base.metadata,
     Column('str8', String(50)),
 )
 
+
 class LogTable(Base):
     __table__ = log_table
 
@@ -58,6 +61,7 @@ class LogTable(Base):
                  self.str4, self.str5, self.str6,
                  self.str7, self.str8))
 
+
 def log_gen(session):
     while True:
         yield True
@@ -68,7 +72,6 @@ def log_gen(session):
                                  str4="blow", str5="bluw", str6="ahua",
                                  str7="nil", str8="nul")
             session.add(log_entry)
-
         session.flush()
 
 
@@ -81,30 +84,24 @@ def process_gen(session):
         query_set = query_set.filter(filter_by_id)
         query_set = query_set.order_by(LogTable.id)
         query_set.count()
-
         for log_entry in query_set:
             # process log
-            
             #print "PROCESSING: %r" % (log_entry,)
-
             # save last processed log
             last_id.value = unicode(log_entry.id)
-        
         #print "Last ID: %r" %(last_id)
         session.flush()
+
 
 if __name__ == "__main__":
     session = Session()
     Base.metadata.drop_all(engine)
     Base.metadata.create_all(engine)
-
     last_id = Config(key=u"last_id", value=u"1")
     session.add(last_id)
     session.flush()
-
     interation1 = log_gen(session)
-    interation2 = process_gen(session)
-    
+    interation2 = process_gen(session)    
     counts = [0 for i in range(0,10)]
 
     for i in range(0,10):
@@ -115,5 +112,4 @@ if __name__ == "__main__":
         counts[i] = gc_count
 
     leak = all([counts[i] > counts[i-1] for i in range(1,10)])
-
     print "sqlalchemy.__version__ = %s, leak = %r" %(__version__, leak)
