8955b339c3effa5247c47d7550266b16b1dc1ef1
Kent Bower <kb@retailarchitects.com>
2020-06-15 09:22:13 -0400

    more index creation tests

--- funcindex.py
+++ funcindex.py
@@ -1,5 +1,8 @@
 from sqlalchemy import *
 from sqlalchemy.orm import *
+from sqlalchemy import __version__ as sqlaver
+
+print("*** sqlalchemy: %s" % sqlaver)
 
 engine = create_engine('postgresql://salespylot:salespylot@localhost:5444/v12?application_name=Loft',echo=True)
 
@@ -14,9 +17,18 @@ i = Index('myfuncindex',
     func.coalesce(table.c.id_b, text("''")),
     unique=True
     )
+    
+i2 = Index('my2colindex',
+    table.c.id_a,
+    table.c.id_b)
+    
 
 metadata.create_all()
 
 i.drop()
+i2.drop()
+
+i.create()
+i2.create()
 
 metadata.drop_all()
